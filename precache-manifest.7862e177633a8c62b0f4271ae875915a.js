10f06a71b8c331b29d28d3b2e177dd32cf27cd78
Mauricio Eugênio <mauricio@digitalbluemarketing.com.br>
2019-02-11 15:33:43 -0200

    Updates

---
+++ precache-manifest.7862e177633a8c62b0f4271ae875915a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73f69330538132a4b130",
+    "url": "/gerador-link-wpp/static/js/runtime~main.73f69330.js"
+  },
+  {
+    "revision": "43f243a54fa47bf72782",
+    "url": "/gerador-link-wpp/static/js/main.43f243a5.chunk.js"
+  },
+  {
+    "revision": "15b2c9e44304fbb280e8",
+    "url": "/gerador-link-wpp/static/js/1.15b2c9e4.chunk.js"
+  },
+  {
+    "revision": "43f243a54fa47bf72782",
+    "url": "/gerador-link-wpp/static/css/main.58a7853f.chunk.css"
+  },
+  {
+    "revision": "afbc98e71cdd5dd14f2e233633e916cd",
+    "url": "/gerador-link-wpp/index.html"
+  }
+];
